4e339ef0e22a5261fdb78c88efe5ac873586104b
Asal Vaghefzadeh <vaghefzadehasal@gmail.com>
2026-09-24 00:06:27 -0400

    jumping

--- scripts/xr_move.gd
+++ scripts/xr_move.gd
@@ -6,7 +6,6 @@
 extends Node3D
 class_name XRMovement
 
-
 ## Kill switch. Turn it off for cutscenes, menus, whatever
 @export var enabled := true
 
@@ -23,19 +22,21 @@ class_name XRMovement
 
 @export_group("Parameters")
 @export var move_speed := 2.0 ## m/s
-@export var snap_degrees := 30.0
 @export_range(0.0, 0.9) var deadzone := 0.2 ## for stick drift
 @export var gravity := 9.8
+## Jump detection stuff
+@export var jump_velocity := 4.5
+@export var jump_detection_threshold := .2  ## m/s upward head speed to count as a jump
+
+@export var turn_speed := 90.0  ## degrees per second, replaces snap_degrees
+
+var _prev_camera_y := 0.0
 
 var _origin: XROrigin3D = null
 var _body: CharacterBody3D = null
 var _camera: XRCamera3D = null
 var _move_controller: XRController3D = null
 var _turn_controller: XRController3D = null
-
-# Latch to stop multiple turns from one input
-var _can_turn := true
-
 
 func _ready() -> void:
 	_origin = get_parent() as XROrigin3D
@@ -53,7 +54,30 @@ func _ready() -> void:
 	_camera = get_node_or_null(camera) as XRCamera3D
 	_move_controller = get_node_or_null(move_controller) as XRController3D
 	_turn_controller = get_node_or_null(turn_controller) as XRController3D
+	
+	if _camera:
+		_prev_camera_y = _camera.transform.origin.y
 
+var _smoothed_vertical_speed := 0.0
+@export_range(0.0, 1.0) var smoothing := 0.3  ## lower = smoother/slower to react, higher = more responsive/noisier
+
+func _detect_physical_jump(delta: float) -> void:
+	if _camera == null:
+		return
+
+	var current_y := _camera.transform.origin.y
+	var raw_speed := (current_y - _prev_camera_y) / delta
+	_prev_camera_y = current_y
+
+	# Exponential moving average to smooth out jitter
+	_smoothed_vertical_speed = lerp(_smoothed_vertical_speed, raw_speed, smoothing)
+
+	#print("smoothed speed: ", _smoothed_vertical_speed, " | on floor: ", _body.is_on_floor())
+
+	if _body.is_on_floor() and _smoothed_vertical_speed > jump_detection_threshold:
+		#print("JUMP TRIGGERED")
+		_body.velocity.y = jump_velocity
+		
 func _physics_process(delta: float) -> void:
 	if not enabled:
 		return
@@ -65,7 +89,9 @@ func _physics_process(delta: float) -> void:
 	_slide(delta)
 
 	# 4. Handle snap turning
-	_snap_turn()
+	_smooth_turn(delta)
+	
+	_detect_physical_jump(delta)   # <-- physical jump detection
 
 	# 5. Actually move, respecting collisions
 	_body.move_and_slide()
@@ -102,28 +128,21 @@ func _slide(delta: float) -> void:
 	_body.velocity.z = direction.z * move_speed
 
 
-func _snap_turn() -> void:
+
+
+func _smooth_turn(delta: float) -> void:
 	if _turn_controller == null:
 		return
 
 	var stick := _turn_controller.get_vector2(stick_action)
-
-	# Allow a turn when the stick is in the deadzone.
 	if absf(stick.x) < deadzone:
-		_can_turn = true
 		return
 
-	if not _can_turn:
-		return
-	_can_turn = false # This is where a turn will happen. We flip this flag to stop another one
-
-	# snap_degrees left or right
-	var angle := deg_to_rad(snap_degrees) * -signf(stick.x)
+	var angle := deg_to_rad(turn_speed) * delta * -stick.x
 
 	var pivot := _camera.global_position
 	pivot.y = _origin.global_position.y
 
-	# Perform the rotation at the origin to avoid translation
 	var t := _origin.global_transform
 	t = t.translated(-pivot)
 	t = Transform3D(Basis(Vector3.UP, angle), Vector3.ZERO) * t
